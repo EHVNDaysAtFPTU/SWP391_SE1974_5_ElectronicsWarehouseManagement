006e94df87e404545a16e71256537991218a1039
friedrich <wquangu@gmail.com>
2026-02-15 16:49:02 +0700

    Add manager controller, DTOs and service for item

---
+++ ElectronicsWarehouseManagement.WebAPI/Controllers/ManagerCtrl.cs
@@ -0,0 +1,44 @@
+﻿using ElectronicsWarehouseManagement.Repositories.Entities;
+using ElectronicsWarehouseManagement.WebAPI.DTO;
+using ElectronicsWarehouseManagement.WebAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ElectronicsWarehouseManagement.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/manager")]
+    [Authorize(Roles ="1")]
+    public class ManagerCtrl:ControllerBase
+    {
+        readonly IManagerService _managerService;
+        private readonly ILogger<ManagerCtrl> _logger;
+        public ManagerCtrl(IManagerService managerService, ILogger<ManagerCtrl> logger)
+        {
+            _managerService = managerService;
+            _logger = logger;
+        }
+        [HttpGet("itemlist")]
+        public async Task<IActionResult> GetItemList()
+        {
+            var result = await _managerService.GetItemList(); if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("request")]
+        public async Task<IActionResult> GetTransferReqList()
+        {
+            var result = await _managerService.GetTransferReqList(); 
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
---
+++ ElectronicsWarehouseManagement.WebAPI/DTO/ItemDTO.cs
@@ -0,0 +1,44 @@
+﻿using ElectronicsWarehouseManagement.Repositories.Entities;
+using System.Text.Json.Serialization;
+
+namespace ElectronicsWarehouseManagement.WebAPI.DTO
+{
+    public class ItemDTO
+    {
+        [JsonPropertyName("item_id")]
+        public int ItemId { get; set; }
+
+        [JsonPropertyName("metadata")]
+        public string Metadata { get; set; }
+
+        [JsonPropertyName("item_quantity")]
+        public int Quantity { get; set; }
+
+        [JsonPropertyName("item_unit")]
+        public string Unit { get; set; }
+
+        [JsonPropertyName("import_date")]
+        public DateOnly ImportDate { get; set; }
+
+        [JsonPropertyName("unit_price")]
+        public float UnitPrice { get; set; }
+
+        [JsonPropertyName("transfer_id")]
+        public int? TransferId { get; set; }
+
+        [JsonPropertyName("iob_id")]
+        public int? IobId { get; set; }
+
+        [JsonPropertyName("iob")]
+        public virtual InOutBoundReq Iob { get; set; }
+
+        [JsonPropertyName("transfer")]
+        public virtual TransferReq Transfer { get; set; }
+
+        [JsonPropertyName("bins")]
+        public virtual ICollection<Bin> Bins { get; set; } = new List<Bin>();
+
+        [JsonPropertyName("categories")]
+        public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
+    }
+}
